34c5278405b402b7a676ed7ff4d3bd571679b44d
Kelven Silva <kelvenjorge.ks@gmail.com>
2024-06-16 13:18:22 +0200

    Atualizacoes

--- src/@types/fastify-cors.d.ts
+++ src/@types/fastify-cors.d.ts
@@ -1,21 +1,21 @@
 declare module 'fastify-cors' {
-    import { FastifyPlugin } from 'fastify';
+    import { FastifyPlugin } from 'fastify'
   
     interface FastifyCorsOptions {
-      origin?: string | boolean | RegExp | (string | RegExp)[] | ((origin: string, cb: (err: Error | null, allow?: boolean) => void) => void);
-      methods?: string | string[];
-      allowedHeaders?: string | string[];
-      exposedHeaders?: string | string[];
-      credentials?: boolean;
-      maxAge?: number;
-      preflightContinue?: boolean;
-      optionsSuccessStatus?: number;
-      preflight?: boolean;
-      strictPreflight?: boolean;
-      hideOptionsRoute?: boolean;
+      origin?: string | boolean | RegExp | (string | RegExp)[] | ((origin: string, cb: (err: Error | null, allow?: boolean) => void) => void)
+      methods?: string | string[]
+      allowedHeaders?: string | string[]
+      exposedHeaders?: string | string[]
+      credentials?: boolean
+      maxAge?: number
+      preflightContinue?: boolean
+      optionsSuccessStatus?: number
+      preflight?: boolean
+      strictPreflight?: boolean
+      hideOptionsRoute?: boolean
     }
   
-    const fastifyCors: FastifyPlugin<FastifyCorsOptions>;
-    export default fastifyCors;
+    const fastifyCors: FastifyPlugin<FastifyCorsOptions>
+    export default fastifyCors
   } 
   
